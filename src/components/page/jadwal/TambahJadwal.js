4a442fec4f5d372ac1f2958fb1b151b24f0aa919
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-07-09 19:39:09 +0700

    fetching data mapel, kelas

--- src/components/page/jadwal/TambahJadwal.js
+++ src/components/page/jadwal/TambahJadwal.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FormControl, InputLabel, Select, MenuItem, Box } from "@mui/material";
+import axios from "axios";
 
 const TambahJadwal = () => {
+  const [formData, setFormData] = useState({
+    guru: "",
+    kelas: "",
+  });
+  const [users, setUsers] = useState();
+  const [kelas, setKelas] = useState();
+  const [mapels, setMapels] = useState();
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const getusers = async () => {
+    const res = await axios.get("/api/users");
+    setUsers(res.data.data);
+  };
+
+  const getKelas = async () => {
+    const res = await axios.get("/api/list/classes");
+    setKelas(res.data.data);
+  };
+
+  const getMapel = async () => {
+    const res = await axios.get("/api/list/mapels");
+    setMapels(res.data.data);
+  };
+
+  useEffect(() => {
+    getusers();
+    getKelas();
+    getMapel();
+  }, []);
+
+  // useEffect(() => {
+  // }, [formData]);
+
   return (
-    <div className="container w-1/2 m-auto border rounded p-2">
+    <div className="container w-1/2 m-auto border rounded p-2 mt-5">
       <Link
         to="/jadawal"
         className="flex bg-slate-400 text-center py-1 px-1 text-white hover:bg-slate-600 rounded w-fit"
@@ -19,8 +56,56 @@ const TambahJadwal = () => {
         Back
       </Link>
       <h2 className="text-center font-semibold">Tambah Jadwal Pelajaran</h2>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="guru">Guru</InputLabel>
+          <Select
+            labelId="guru"
+            id="guru"
+            name="guru"
+            label="Guru"
+            onChange={(e) => handleChange(e)}
+          >
+            {users?.map((e) => (
+              <MenuItem value={e.id}>{e.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <div className="mt-2">
+          <FormControl fullWidth classes="mt-2">
+            <InputLabel id="kelas">Kelas</InputLabel>
+            <Select
+              labelId="kelas"
+              id="kelas"
+              name="kelas"
+              label="Kelas"
+              onChange={(e) => handleChange(e)}
+            >
+              {kelas?.map((e) => (
+                <MenuItem value={e.id}>{e.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="mt-2">
+          <FormControl fullWidth classes="mt-2">
+            <InputLabel id="mapel">Mata Pelajaran</InputLabel>
+            <Select
+              labelId="mapel"
+              id="mapel"
+              name="mapel"
+              label="Mata Pelajaran"
+              onChange={(e) => handleChange(e)}
+            >
+              {mapels?.map((e) => (
+                <MenuItem value={e.id}>{e.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </Box>
     </div>
   );
 };
 
-export default TambahJadwal
+export default TambahJadwal;
